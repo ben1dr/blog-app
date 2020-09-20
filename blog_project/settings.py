9d56b1fb8127fbd8d6240d09dbfbd8a2ad65255a
Beny <benyamin.derakhshani@gmail.com>
2020-09-20 20:44:42 +0430

    forms and user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',  # new
+    'django.contrib.staticfiles',  # new
+    'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
